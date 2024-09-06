e645ac6a3deb52c6894c3167b19488c90a6dc0bd
arthur <arthur.rios007@gmail.com>
2024-09-05 21:35:25 -0300

    Fundamentos do React Native - Criando componente Option

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,6 +4,7 @@ import { MaterialIcons } from '@expo/vector-icons'
 import { colors } from '@/styles/colors'
 import { Categories } from '@/components/categories'
 import { Link } from '@/components/link'
+import { Option } from '@/components/option'
 
 export default function Index() {
   return (
@@ -44,7 +45,7 @@ export default function Index() {
 
               <TouchableOpacity>
                 <MaterialIcons 
-                  name='close'
+                  name="close"
                   size={20}
                   color={colors.gray[400]}
                 />
@@ -58,6 +59,11 @@ export default function Index() {
             <Text style={styles.modalUrl}>
               https://www.rocketseat.com.br/
             </Text>
+
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
           </View>
         </View>
       </Modal>
