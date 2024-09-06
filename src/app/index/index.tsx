23eadc82d68ae6cd8d05fe9f04ef4da98f006b2f
arthur <arthur.rios007@gmail.com>
2024-09-05 21:24:21 -0300

    Fundamentos do React Native - Utilizando Modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,7 @@
-import { FlatList, Image, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, Modal, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
 import { MaterialIcons } from '@expo/vector-icons'
 import { colors } from '@/styles/colors'
-import { Category } from '@/components/category'
-import { categories } from '@/utils/categories'
 import { Categories } from '@/components/categories'
 import { Link } from '@/components/link'
 
@@ -37,6 +35,32 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+
+              <TouchableOpacity>
+                <MaterialIcons 
+                  name='close'
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            
+            <Text style={styles.modalLinkName}>
+              Rocketseat
+            </Text>
+
+            <Text style={styles.modalUrl}>
+              https://www.rocketseat.com.br/
+            </Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
